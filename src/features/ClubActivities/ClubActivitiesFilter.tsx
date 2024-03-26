99203ce9abbcac31abf8f3b7375d84608477d613
Fabián Hernández <fhernandezn@gmail.com>
2024-03-26 13:23:18 -0500

    NEEDS REVIEW: get unique sport_type options for Dropdown Filter
    Take a look on MUI anoiying warning
    Forced to use Any Type on map and redice options logic

--- src/features/ClubActivities/ClubActivitiesFilter.tsx
+++ src/features/ClubActivities/ClubActivitiesFilter.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { DropdownFilter, SliderRangeFilter } from '../Filter/Index';
-import { ActivitySportType } from '../../models/StravaModels';
 import './ClubActivities.css';
 
 type FilterClubActivitiesProps = {
@@ -9,14 +8,12 @@ type FilterClubActivitiesProps = {
 
 const FilterClubActivities: React.FC<FilterClubActivitiesProps> = ({title}: FilterClubActivitiesProps) => {
 
-    const sportTypes = Object.keys(ActivitySportType) as unknown as ActivitySportType[];
-    
     return (
         <>
             <h2>{title}</h2>
             <SliderRangeFilter name="Distance" field="distance" />
             <SliderRangeFilter name="Total Elevation Gain" field="total_elevation_gain" />
-            <DropdownFilter className="dropdown full-width" name="Sport Type" field="sport_type" options={sportTypes} />
+            <DropdownFilter className="dropdown full-width" name="Sport Type" field="sport_type" />
         </>
         )
     }
--- src/features/Filter/Dropdown.tsx
+++ src/features/Filter/Dropdown.tsx
@@ -1,21 +1,37 @@
 import { FormControl, InputLabel, MenuItem, Select, type SelectChangeEvent } from "@mui/material";
 import { ActivitySportType, ClubActivity } from "../../models/StravaModels";
-import { ComponentPropsWithoutRef, useState } from "react";
+import { ComponentPropsWithoutRef, useEffect, useState } from "react";
 import { useStravaContext } from "../../store/StravaContext";
 import { useLayoutContext } from "../../store/LayoutContext";
 
 type DropdownFilterProps = {
     name: string;
     field: string;
-    options: ActivitySportType[];
 } & ComponentPropsWithoutRef<'div'>;
 
 
-const DropdownFilter: React.FC<DropdownFilterProps> = ({name, field, options, ...props}) => {
+const DropdownFilter: React.FC<DropdownFilterProps> = ({name, field, ...props}) => {
 
     const { updateGridData } = useLayoutContext();
     const { clubActivities } = useStravaContext();
+
+    const [options, setOptions] = useState<ActivitySportType[]>([])
     const [sportType, setSportType] = useState<ActivitySportType>(ActivitySportType.AlpineSki);
+
+    useEffect(() => {
+        const options = getUniqueSportTypesOptions();
+        setOptions(options);
+    }, [clubActivities]);
+
+    const getUniqueSportTypesOptions = () => {
+        if (!clubActivities) { return [] };
+
+        const sportTypes = clubActivities?.map<ActivitySportType[]>((activity: any) => (activity.sport_type))
+
+        return sportTypes?.reduce<ActivitySportType[]>((list, option: any) => {
+            return !list.includes(option) ? [...list, option] : list;
+        }, []) as ActivitySportType[];
+    }
 
     const handleSelectChange = (event: SelectChangeEvent) => {
         const selected = event.target.value as unknown as ActivitySportType;
@@ -28,7 +44,7 @@ const DropdownFilter: React.FC<DropdownFilterProps> = ({name, field, options, ..
         return activities.filter(activity => activity.sport_type === selected);
     }
 
-    return (
+    return options ? (
         <>
             <h3>{ name }</h3>
             <FormControl className={props.className}>
@@ -46,7 +62,7 @@ const DropdownFilter: React.FC<DropdownFilterProps> = ({name, field, options, ..
                 </Select>
             </FormControl>
         </>
-    )
+    ) : null;
 }
 
 export default DropdownFilter;
